b25427bc04628495c30693a5d5b1df21cded1c59
KikyTokamuro <kiky.tokamuro@yandex.ru>
2024-03-12 20:23:25 +0300

    Rewrited old emacs config to literate style

---
+++ init.el
@@ -0,0 +1,9 @@
+;;; init.el --- Emacs init
+;; Copyright (C) 2021-2024 Arhangelsky Daniil (Kiky Tokamuro)
+;; Author: Arhangelsky Daniil (Kiky Tokamuro) <kiky.tokamuro@yandex.ru>
+;;; Commentary:
+;;; Code:
+
+(require 'org)
+
+(org-babel-load-file (concat user-emacs-directory "README.org"))
